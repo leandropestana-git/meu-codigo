b1f1d22609557f5b876769bbacb975aed3a3adf9
leandropestana-git <leandropestana.lv.lv@gmail.com>
2025-09-14 09:52:04 -0300

    Create atividade 2.py
    1a atividade

---
+++ atividade 2.py
@@ -0,0 +1,14 @@
+idade = 31
+leandro31 ="minha idade "
+#print (leandro31 + str (idade))
+
+
+leandroaltu ="1.84"
+idade += float (leandroaltu)
+#print ("e minha altura "+leandroaltu+" somada a minha idade resulta em "+  str (idade))
+
+
+alturabooli = True
+idadebooli = False
+print (" minha altura esta "+ str (alturabooli)+" e minha idade esta "+ str (idadebooli))
+
