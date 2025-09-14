37bd491e00f17ca57ef80b11ca8cf0ebb5210989
leandropestana-git <leandropestana.lv.lv@gmail.com>
2025-09-14 09:53:58 -0300

    Create atividade1.py
    1a atividade

---
+++ atividade1.py
@@ -0,0 +1,15 @@
+leandroVieira='leandro'
+print("hello WORLD "+leandroVieira)
+sakuraHaruno='sakura'
+print("hello world "+ sakuraHaruno)
+
+
+numero1="7"
+numero2="6"
+print ("leandro tem " + numero1 + " sakura tem "+ numero2)
+
+
+modolo=7%6
+print("quatidade de letra")
+print(modolo)
+
