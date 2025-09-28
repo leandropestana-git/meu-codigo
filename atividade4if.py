6d956a02da5c8f5ce1b6d82e108b0b4840d45373
leandropestana-git <leandropestana.lv.lv@gmail.com>
2025-09-28 11:57:30 -0300

    Create atividade4if.py
    atividade4.py

---
+++ atividade4if.py
@@ -0,0 +1,9 @@
+chuva = False
+nublado = True
+if chuva == True:
+    print ("levar o guarda-chuva")
+else:
+    if nublado == True:
+        print("levar assim mesmo o guarda-chuva")
+    else:
+        print("não levar o guarda-chuva")
