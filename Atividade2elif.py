46b6b3495512095a0d331267ed0dace4eb704770
leandropestana-git <leandropestana.lv.lv@gmail.com>
2025-10-19 10:47:09 -0300

    Create Atividade2elif.py
    atividade2elif.py

---
+++ Atividade2elif.py
@@ -0,0 +1,11 @@
+nota =float(input('escreva nota: '))
+if nota<6:
+    print ('nota F')
+elif nota<7:
+    print ('nota D')
+elif nota<8:
+    print('nota C') 
+elif nota<9:
+    print('nota B')
+elif nota<=10:
+    print('nota A')       
