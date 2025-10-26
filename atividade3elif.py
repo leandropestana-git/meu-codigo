e237bcb913a935f6eef18a958ed515abecc6d0b6
leandropestana-git <leandropestana.lv.lv@gmail.com>
2025-10-26 12:13:29 -0300

    Update and rename atividade3.elif to atividade3elif.py
    Update and rename atividade3.elif to atividade3elif.py

--- atividade3elif.py
+++ atividade3elif.py
@@ -2,3 +2,4 @@ idade = int(input("digite sua idade: "))
 ingresso = bool(input("tem ingresso? "))
 autorizaçaodospais = bool(input("tem autorizaçao dos pais? "))
 listavip = bool(input("entrar mesmo sem ingresso? "))
+if idade => 18 and ingresso: 
