190c59dd1eb66fc95efd762abdf9ff2651debc77
leandropestana-git <leandropestana.lv.lv@gmail.com>
2025-10-26 13:45:13 -0300

    Update atividade3elif.py
    acabou

--- atividade3elif.py
+++ atividade3elif.py
@@ -2,4 +2,11 @@ idade = int(input("digite sua idade: "))
 ingresso = bool(input("tem ingresso? "))
 autorizaçaodospais = bool(input("tem autorizaçao dos pais? "))
 listavip = bool(input("entrar mesmo sem ingresso? "))
-if idade => 18 and ingresso: 
+if idade >=18 and  (ingresso or vip):
+  print ("pode entrar")
+elif idade <12 :
+print ("nao pode entrar ") 
+elif idade <18 and autorizaçaodospais and (ingresso or vip) :
+  print (" pode entrar ")
+else :
+  print ("nao pode entrar ") 
