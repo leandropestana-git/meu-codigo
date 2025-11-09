047feae35a0479696612e70a1d2e6af863995534
leandropestana-git <leandropestana.lv.lv@gmail.com>
2025-11-09 12:01:18 -0300

    Create atividadeextra.py
     atividadeextra.py

---
+++ atividadeextra.py
@@ -0,0 +1,6 @@
+idade = int(input("digite sua idade: "))
+passaportedeavião = int(input("idade da passagero:"))
+climabom = int(input("viaja bom:"))
+climaruim = int(input("viaja ruim:"))
+
+if idade 
