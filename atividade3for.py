253620238eaa07cfcbe5df8eba084981864f1dd0
leandropestana-git <leandropestana.lv.lv@gmail.com>
2025-11-30 10:49:53 -0300

    Create atividade3for.py
    atividade3for.py

---
+++ atividade3for.py
@@ -0,0 +1,8 @@
+listadejogo = ["final fantasy xv", "shadow of the colossus","duck hunt","hogwarts legacy" ]
+for meujogo in listadejogo: 
+    if meujogo == "final fantasy xv":
+        print ( meujogo + " é o jogo perfeito")
+    elif  meujogo == "duck hunt":
+        print ( meujogo + " é o jogo odio")  
+    else:
+        print ( meujogo + " é o jogo legel")    
