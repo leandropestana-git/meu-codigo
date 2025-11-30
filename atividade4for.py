fd1e7512c30ec85e7473d115e9837cee0041c0b9
leandropestana-git <leandropestana.lv.lv@gmail.com>
2025-11-30 11:50:53 -0300

    Create atividade4for.py
     atividade4for.py

---
+++ atividade4for.py
@@ -0,0 +1,18 @@
+palavra = input("digite sua palavra: ")
+for letra in palavra :
+    if letra == "l":
+      print (letra + " está no meu nome") 
+    elif letra == "e":
+       print (letra + " está no meu nome")
+    elif letra == "a":
+       print (letra + " está no meu nome") 
+    elif letra == "n":
+       print (letra + " está no meu nome")
+    elif letra == "d":
+       print (letra + " está no meu nome") 
+    elif letra == "r":
+       print (letra + " está no meu nome") 
+    elif letra == "o":
+       print (letra + " está no meu nome")  
+    else:
+       print (letra + " não está no meu nome")
